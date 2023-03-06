b839d89283919104aa3cb1506d6f93c4d439ec23
klofac <klofac@ipex.cz>
2023-03-06 23:41:26 +0100

    ADD - import chybejicich ticketu podle analyzy v pomocne DB

---
+++ importMissingTickets.php
@@ -0,0 +1,66 @@
+<?php
+require "config.php";
+
+/**
+ * Call hooknt.php 
+ */
+function call_hooknt(string $ticketCode) : string {
+
+    $command = $GLOBALS['config_hooknt_url'].'?ticketCode='.$ticketCode;
+    //echo $command."<BR/>\n";
+
+    $cht = curl_init($command);
+    
+    curl_setopt($cht, CURLOPT_HEADER, 0);
+    curl_setopt($cht, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($cht, CURLOPT_HTTPHEADER, array(
+        'Content-type: text/html'
+    ));
+
+    $curloutput = curl_exec($cht);
+
+    if(curl_error($cht)) {
+        echo "ERROR: ".curl_error($cht);
+    }
+
+    curl_close($cht);
+
+    return $curloutput;    
+}
+
+
+
+/**
+ * 
+ * MAIN PROGRAM
+ * 
+ */
+    $time_start = microtime(true);
+
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $mysqli = new mysqli($GLOBALS['config_tmpDB_hostname'], $GLOBALS['config_tmpDB_user'], $GLOBALS['config_tmpDB_pwd'], $GLOBALS['config_tmpDB_db'],3306);
+    $mysqli->set_charset('utf8');
+
+    $command = "SELECT laTicketID FROM ".$GLOBALS['config_tmpDB_missingTickets']." WHERE zapsanoDoHLP IS NULL ORDER BY laTicketChanged DESC LIMIT 1";
+    //echo $command."<BR/>";
+
+    $tickets = $mysqli->query($command);
+    
+    foreach($tickets as $key => $ticket) {
+
+        $ticketCode = $ticket["laTicketID"];
+        $hookntResult = call_hooknt($ticketCode);  
+
+        echo $hookntResult."<BR/>\n";
+
+        if(true) {
+            $commandUpdate = "UPDATE ".$GLOBALS['config_tmpDB_missingTickets']." SET zapsanoDoHLP=NOW() WHERE laTicketID='".$ticketCode."' LIMIT 1";
+            //echo $commandUpdate."<BR/>\n";
+            $mysqli->query($commandUpdate);
+        }
+        sleep(2);
+    }
+    $time_end = microtime(true);
+
+    echo "Finished in ".round($time_end - $time_start)." sec";
+?>
